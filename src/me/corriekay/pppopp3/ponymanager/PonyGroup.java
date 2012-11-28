ca89350e68f370db1fab14eb0551d382e2a76db6
CorriePie <corriekay.pinkie@outlook.com>
2012-11-27 16:23:34 -0800

    added object to handle groups. This object holds permissions for specific groups, global perms, and world specific permissions.
    Signed-off-by: CorriePie <corriekay.pinkie@outlook.com>

---
+++ src/me/corriekay/pppopp3/ponymanager/PonyGroup.java
@@ -0,0 +1,77 @@
+package me.corriekay.pppopp3.ponymanager;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+import org.bukkit.World;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+
+public class PonyGroup {
+
+	protected final HashMap<String,BunchOfPerms> worldPerms = new HashMap<String,BunchOfPerms>();
+	public final String perm2add2;
+	private String name;
+	
+	public PonyGroup(FileConfiguration config, String name){
+		this.name = name;
+		perm2add2 = config.getString("group."+name+".perm2add2");
+		HashMap<String,Boolean> globalPerms = new HashMap<String,Boolean>();
+		{//get global perms
+			HashSet<String> negPerms = new HashSet<String>();
+			for(String gperm : config.getStringList("groups."+name+".globalPermissions")){
+				if(gperm.startsWith("-")){
+					negPerms.add(gperm.substring(1,gperm.length()));
+				} else {
+					globalPerms.put(gperm,true);
+				}
+			}
+			for(String negPerm : negPerms){
+				globalPerms.put(negPerm,false);
+			}
+		}
+		{//get world perms, add to worldPerms
+			for(String worldString : config.getConfigurationSection("groups."+name+".worldPermissions").getKeys(false)){
+				BunchOfPerms worldBunchPerms;
+				HashMap<String,Boolean> finalWorldPerms = new HashMap<String,Boolean>();
+				for(String perm : globalPerms.keySet()){
+					finalWorldPerms.put(perm, globalPerms.get(perm));
+				}
+				HashSet<String> negPerms = new HashSet<String>();
+				for(String worldPermission : config.getStringList("groups."+name+".worldPermissions."+worldString)){
+					if(worldPermission.startsWith("-")){
+						negPerms.add(worldPermission.substring(1,worldPermission.length()));
+					} else {
+						finalWorldPerms.put(worldPermission, true);
+					}
+				}
+				for(String negPerm : negPerms){
+					finalWorldPerms.put(negPerm, false);
+				}
+				worldBunchPerms = new BunchOfPerms(finalWorldPerms);
+				worldPerms.put(worldString, worldBunchPerms);
+			}
+		}
+	}
+	protected HashMap<String,Boolean> getPermissions(World w){
+		return getPermissions(w.getName());
+	}
+	protected HashMap<String,Boolean> getPermissions(String w){
+		return worldPerms.get(w).getPerms();
+	}
+	protected boolean canMoveTo(Player asker){
+		return asker.hasPermission(perm2add2);
+	}
+	protected boolean canMoveTo(ConsoleCommandSender asker){
+		return asker.hasPermission(perm2add2);
+	}
+	protected boolean canMoveTo(CommandSender asker){
+		return asker.hasPermission(perm2add2);
+	}
+	protected String getName(){
+		return name;
+	}
+
+}
