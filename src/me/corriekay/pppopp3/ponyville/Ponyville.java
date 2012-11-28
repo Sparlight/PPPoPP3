988a35834e140a69888f4c5fbc6d02d344749c67
CorriePie <corriekay.pinkie@outlook.com>
2012-11-27 16:19:24 -0800

    added method for getting an offline pony object. If the pony is online, it returns the instance already loaded into the server. if not, it opens a new instance
    Signed-off-by: CorriePie <corriekay.pinkie@outlook.com>

--- src/me/corriekay/pppopp3/ponyville/Ponyville.java
+++ src/me/corriekay/pppopp3/ponyville/Ponyville.java
@@ -1,6 +1,7 @@
 package me.corriekay.pppopp3.ponyville;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.HashMap;
 
 import me.corriekay.pppopp3.Mane;
@@ -56,16 +57,30 @@ public final class Ponyville extends PSCmdExe{
 		pv.ponies.put(p.getName(), po);
 		return po;
 	}
-	
+	public static Pony getOfflinePony(Player p ){
+		return getOfflinePony(p.getName());
+	}
+	public static Pony getOfflinePony(String p){
+		Pony pony = getPony(p);
+		if(pony == null){
+			try {
+				return new Pony(p);
+			} catch (FileNotFoundException e) {
+				return null;
+			}
+		} else {
+			return pony;
+		}
+	}
 	@EventHandler
 	public void onJoin(PlayerJoinEvent event){
-		JoinEvent je = new JoinEvent(addPony(event.getPlayer()),true);
+		JoinEvent je = new JoinEvent(event.getPlayer(),addPony(event.getPlayer()),true);
 		Bukkit.getPluginManager().callEvent(je);
 		event.setJoinMessage(je.getMsg());
 	}
 	@EventHandler
 	public void onQuit(PlayerQuitEvent event){
-		QuitEvent qe = new QuitEvent(getPony(event.getPlayer()),true);
+		QuitEvent qe = new QuitEvent(event.getPlayer(),getPony(event.getPlayer()),true);
 		Bukkit.getPluginManager().callEvent(qe);
 		event.setQuitMessage(qe.getMsg());
 		Pony p = getPony(event.getPlayer());
