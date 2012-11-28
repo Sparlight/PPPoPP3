cdaa5138a14b41cbb44672e3bf64c5e029c3c207
CorriePie <corriekay.pinkie@outlook.com>
2012-11-27 16:22:44 -0800

    added perms object.
    Signed-off-by: CorriePie <corriekay.pinkie@outlook.com>

---
+++ src/me/corriekay/pppopp3/ponymanager/BunchOfPerms.java
@@ -0,0 +1,16 @@
+package me.corriekay.pppopp3.ponymanager;
+
+import java.util.HashMap;
+
+public class BunchOfPerms {
+
+	private HashMap<String,Boolean> perms;
+
+	public BunchOfPerms(HashMap<String,Boolean> perms){
+		this.perms = perms;
+	}
+	
+	public HashMap<String,Boolean> getPerms(){
+		return perms;
+	}
+}
