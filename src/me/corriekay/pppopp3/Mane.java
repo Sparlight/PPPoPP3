44a5bbb683e980cce5535e17149e91226b733d8e
CorriePie <corriekay.pinkie@outlook.com>
2012-12-03 17:34:07 -0800

    added world edit hook module.
    Signed-off-by: CorriePie <corriekay.pinkie@outlook.com>

--- src/me/corriekay/pppopp3/Mane.java
+++ src/me/corriekay/pppopp3/Mane.java
@@ -5,6 +5,7 @@ import java.util.HashSet;
 import me.corriekay.pppopp3.modules.EntityLogger;
 import me.corriekay.pppopp3.modules.Equestria;
 import me.corriekay.pppopp3.modules.RemoteChest;
+import me.corriekay.pppopp3.modules.WorldEditHookModule;
 import me.corriekay.pppopp3.ponymanager.PonyManager;
 import me.corriekay.pppopp3.ponyville.Ponyville;
 import me.corriekay.pppopp3.utils.PSCmdExe;
@@ -27,6 +28,7 @@ public class Mane extends JavaPlugin{
 		modules.add(new PonyManager());
 		modules.add(new RemoteChest());
 		try {modules.add(new EntityLogger());} catch (Exception e){e.printStackTrace();}
+		modules.add(new WorldEditHookModule());
 	}
 	public void onDisable(){
 		
