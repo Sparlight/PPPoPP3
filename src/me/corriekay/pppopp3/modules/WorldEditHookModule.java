3c6bd3376a7a082d60ca3caebc8e643aa53abc59
CorriePie <corriekay.pinkie@outlook.com>
2012-12-05 23:03:39 -0800

    Massive overhaul to the module
    Signed-off-by: CorriePie <corriekay.pinkie@outlook.com>

--- src/me/corriekay/pppopp3/modules/WorldEditHookModule.java
+++ src/me/corriekay/pppopp3/modules/WorldEditHookModule.java
@@ -19,11 +19,10 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
-import org.bukkit.event.world.ChunkLoadEvent;
 import org.bukkit.scheduler.BukkitScheduler;
 
 import com.sk89q.worldedit.BlockVector;
-import com.sk89q.worldedit.IncompleteRegionException;
+import com.sk89q.worldedit.bukkit.WorldEditPlugin;
 import com.sk89q.worldedit.bukkit.selections.CuboidSelection;
 import com.sk89q.worldedit.bukkit.selections.Selection;
 import com.sk89q.worldedit.regions.Region;
@@ -36,10 +35,18 @@ public class WorldEditHookModule extends PSCmdExe {
 	private final HashMap<String, Selection> playerAreas = new HashMap<String, Selection>();
 	private final FileConfiguration config;
 	private final LogBlockQuestioner questioner = (LogBlockQuestioner)Bukkit.getPluginManager().getPlugin("LogBlockQuestioner");
-	//private final WorldEditPlugin wep = ((WorldEditPlugin)Bukkit.getPluginManager().getPlugin("WorldEdit"));
+	private final WorldEditPlugin wep = ((WorldEditPlugin)Bukkit.getPluginManager().getPlugin("WorldEdit"));
 	public WorldEditHookModule(){
-		super("WorldEditHookModule", "registercreativearea","unregistercreativearea");
+		super("WorldEditHookModule", "claimplot", "releaseplot");
 		config = getNamedConfig("wehook.yml");
+		commands.add("//set");
+		commands.add("//replace");
+		commands.add("//overlay");
+		commands.add("//walls");
+		commands.add("//outline");
+		commands.add("//faces");
+		commands.add("//sel");
+		commands.add("/;");
 		loadAreas();
 	}
 	private void loadAreas(){
@@ -65,12 +72,12 @@ public class WorldEditHookModule extends PSCmdExe {
 		}
 		final Player player = (Player)sender;
 		final BukkitScheduler s = Bukkit.getScheduler();
-		if(cmd.getName().equals("registercreativearea")){
+		if(cmd.getName().equals("claimplot")){
 			s.scheduleAsyncDelayedTask(Mane.getInstance(), new Runnable(){
 				public void run(){
 					Selection sel = playerAreas.get(player.getName());
 					if(sel != null){
-						String answer = questioner.ask(player, pinkieSays+"You already have a claimed area! Are you sure you wish to abandon it for a new one?", "yes","no");
+						String answer = questioner.ask(player, pinkieSays+"You already have a creative plot! Are you sure you wish to abandon it for a new one?", "yes","no");
 						if(answer.equals("no")){
 							sendSyncMessage(player,"Okay! Aborting!");
 							return;
@@ -80,7 +87,7 @@ public class WorldEditHookModule extends PSCmdExe {
 								sendSyncMessage(player,"Okay! Aborting!");
 								return;
 							} else if(answer2.equals("yes")){
-								sendSyncMessage(player,"Alright! Removing old creative claim and setting the new one!");
+								sendSyncMessage(player,"Alright! Removing old plot claim and setting the new one!");
 								s.scheduleSyncDelayedTask(Mane.getInstance(),new Runnable(){
 									public void run(){
 										try {
@@ -97,7 +104,7 @@ public class WorldEditHookModule extends PSCmdExe {
 					} else {
 						s.scheduleSyncDelayedTask(Mane.getInstance(), new Runnable(){
 							public void run(){
-								sendMessage(player,"Claiming area!");
+								sendMessage(player,"Claiming plot!");
 								try {
 									addProtection(player);
 								} catch (Exception e) {
@@ -111,15 +118,15 @@ public class WorldEditHookModule extends PSCmdExe {
 				}
 			});
 		}
-		if(cmd.getName().equals("unregistercreativearea")){
+		if(cmd.getName().equals("releaseplot")){
 			Selection sel = playerAreas.get(player.getName());
 			if(sel == null){
-				sendMessage(player,"Silly, you dont have a registered creative area!");
+				sendMessage(player,"Silly, you dont have a creative plot!");
 				return true;
 			}
 			s.scheduleAsyncDelayedTask(Mane.getInstance(), new Runnable(){
 				public void run(){
-					String answer = questioner.ask(player, pinkieSays+"Are you sure you want to remove your creative registered area? Players will be able to edit what you've created there!", "yes", "no");
+					String answer = questioner.ask(player, pinkieSays+"Are you sure you want to remove your plot? Players will be able to edit what you've created there!", "yes", "no");
 					if(answer.equals("no")){
 						sendSyncMessage(player,"Okay! Aborting!");
 						return;
@@ -132,6 +139,7 @@ public class WorldEditHookModule extends PSCmdExe {
 							s.scheduleSyncDelayedTask(Mane.getInstance(), new Runnable(){
 								public void run(){
 									try {
+										sendMessage(player,"Releaseing the plot!");
 										removeProtection(player.getName());
 									} catch (Exception e) {
 										// TODO Auto-generated catch block
@@ -163,89 +171,142 @@ public class WorldEditHookModule extends PSCmdExe {
 			}
 		}
 	}
-	private boolean addProtection(Player player) throws Exception{/* TODO FIX DIS SHIT
+	private boolean addProtection(Player player) throws Exception{
 		Location l = player.getLocation();
 		l.setY(1);
 		
 		//get selection
-		Location one, two, three, four;
-		one = new Location(l.getWorld(),l.getX()-50,1,l.getZ()-50);
-		two = new Location(l.getWorld(),l.getX()-50,1,l.getZ()+50);
-		three = new Location(l.getWorld(),l.getX()+50,1,l.getZ()+50);
-		four = new Location(l.getWorld(),l.getX()+50,1,l.getZ()-50);
-		CuboidSelection finalsel = new CuboidSelection(l.getWorld(),one, new Location(three.getWorld(),three.getBlockX(),three.getWorld().getMaxHeight()-1,three.getBlockZ()));
+		Location one, two;
+		one = new Location(l.getWorld(),l.getBlockX()-50, 1, l.getBlockZ()-50);
+		two = new Location(l.getWorld(),l.getBlockX()+50, 1, l.getBlockZ()+50);
+		Selection bedrocksel = new CuboidSelection(l.getWorld(),one,two);
+		Selection finalsel = new CuboidSelection(l.getWorld(),one,new Location(l.getWorld(),two.getBlockX(),l.getWorld().getMaxHeight()-1,two.getBlockZ()));
 		
+		//compare if it already built stuff / intersects
 		for(BlockVector bv : finalsel.getRegionSelector().getRegion()){
 			Block b = l.getWorld().getBlockAt(bv.getBlockX(),bv.getBlockY(),bv.getBlockZ());
 			Material m = b.getType();
 			if(!(m==Material.BEDROCK||m==Material.AIR||m==Material.GRASS||m==Material.DIRT)){
-				System.out.println(m.name());
+				//System.out.println(m.name());
 				sendMessage(player,"This area has built stuff on it already! It is unclaimable! (hint: there needs to be a 50 block radius in unclaimed, untouched area to claim an area!)");
 				return false;
 			}
-			
 		}
-		
-		//compare if it intersects
-		for(Selection otherSel : playerAreas.values()){
-			if(otherSel.contains(one)||otherSel.contains(two)||otherSel.contains(three)||otherSel.contains(four)){
+		for (Selection othersel : playerAreas.values()){
+			if(intersects(othersel,finalsel)){
 				sendMessage(player,"This area is intersecting an already claimed area! It is unclaimable! (hint: there needs to be a 50 block radius in unclaimed, untouched area to claim an area!)");
 				return false;
 			}
 		}
 		
+		
 		//All checks out! lets lay down the bedrock, and commit the selection
-		CuboidSelection sel = new CuboidSelection(l.getWorld(),one,two);
-		makeBedrock(sel.getRegionSelector().getRegion(),l.getWorld());
-		sel = new CuboidSelection(l.getWorld(),one,four);
-		makeBedrock(sel.getRegionSelector().getRegion(),l.getWorld());
-		sel = new CuboidSelection(l.getWorld(),three, four);
-		makeBedrock(sel.getRegionSelector().getRegion(),l.getWorld());
-		sel = new CuboidSelection(l.getWorld(),three, two);
-		makeBedrock(sel.getRegionSelector().getRegion(),l.getWorld());
+		makeBedrock(bedrocksel,l.getWorld());
 		
 		config.set(player.getName()+".x", l.getBlockX());
 		config.set(player.getName()+".z", l.getBlockZ());
 		saveNamedConfig("wehook.yml",config);
 		
 		loadAreas();
-		*/
 		return true;
 	}
-	private void makeBedrock(Region bvs, World w){
+    private boolean intersects(Selection s1, Selection s2){
+        Location s1Min, s1Max, s2Min, s2Max;
+        s1Min = s1.getMinimumPoint();
+        s1Max = s1.getMaximumPoint();
+        s2Min = s2.getMinimumPoint();
+        s2Max = s2.getMaximumPoint();
+
+        /* The following code checks for collisions along the XZ plane, ignoring Y */          
+
+        if((s1Min.getBlockX() <= s2Max.getBlockX()) && (s2Min.getBlockX() <= s1Max.getBlockX())) //are their X's colliding?
+        {
+                if((s1Min.getBlockZ() <= s2Max.getBlockZ()) && (s2Min.getBlockZ() <= s2Max.getBlockZ())) //are their Z's colliding?
+                {
+                        return true;
+                }
+        }
+        return false;
+}
+	private void makeBedrock(Selection sel, World w) throws Exception{
+		int maxX, minX, maxZ, minZ;
+		maxX = Math.max(sel.getMinimumPoint().getBlockX(), sel.getMaximumPoint().getBlockX());
+		minX = Math.min(sel.getMinimumPoint().getBlockX(), sel.getMaximumPoint().getBlockX());
+		maxZ = Math.max(sel.getMinimumPoint().getBlockZ(), sel.getMaximumPoint().getBlockZ());
+		minZ = Math.min(sel.getMinimumPoint().getBlockZ(), sel.getMaximumPoint().getBlockZ());
+		
+		Region bvs = sel.getRegionSelector().getRegion();
 		for(BlockVector bv : bvs){
 			Block b = w.getBlockAt(bv.getBlockX(),bv.getBlockY(),bv.getBlockZ());
-			b.setType(Material.BEDROCK);
+			int x, z;
+			x = b.getX();
+			z = b.getZ();
+			//if along min/max z
+			if(z == maxZ || z == minZ){
+				if(x>=minX||x<=maxX){
+					b.setType(Material.BEDROCK);
+				}
+			}
+			if(x == maxX || x == minX){
+				if(z>=minZ||z<=maxZ){
+					b.setType(Material.BEDROCK);
+				}
+			}
 		}
 	}
 	@EventHandler
 	public void onCommand(PlayerCommandPreprocessEvent event){
+		if(event.getPlayer().hasPermission("pppopp3.creativeadmin")){
+			return;
+		}
 		String[] msgWords = event.getMessage().split(" ");
 		String cmd = msgWords[0];
 		String[] args = new String[msgWords.length-1];
 		for(int i = 1; i<msgWords.length;i++){
 			args[i-1] = msgWords[i];
 		}
-		if(commands.contains(cmd)){
-			//we command
+		if(cmd.equals("//;")||cmd.equals("/;")){
+			event.setCancelled(true);
+			return;
 		}
-	}
-	@EventHandler
-	public void chunkCreate(ChunkLoadEvent event) throws IncompleteRegionException{
-		if(event.isNewChunk()){
-			if(event.getWorld().getName().equals("equestria")){
-				Selection cr = new CuboidSelection(event.getWorld(),
-						event.getChunk().getBlock(0, 0, 0).getLocation(),
-						event.getChunk().getBlock(15, event.getWorld().getMaxHeight()-1,15).getLocation());
-				for(BlockVector bv : cr.getRegionSelector().getRegion()){
-					Block b = event.getWorld().getBlockAt(bv.getBlockX(),bv.getBlockY(),bv.getBlockZ());
-					if(b.getY()==0){
-						b.setType(Material.BEDROCK);
-					} else if(b.getY()==1){
-						b.setType(Material.GRASS);
-					} else {
-						b.setType(Material.AIR);
-					}
+		if(!event.getPlayer().getWorld().getName().equals("equestria")){
+			return;
+		}
+		if(commands.contains(cmd)){
+			final Player p = event.getPlayer();
+			final Selection sel = wep.getSelection(p);
+			if(cmd.equals("//sel")||cmd.equals("/;")){
+				if(args.length>0&&!(args[0].equals("cuboid"))){
+					sendMessage(p,"Please only use cuboid selections!");
+					event.setCancelled(true);
+					return;
+				}
+				return;
+			}
+			if(sel==null){
+				sendMessage(p,"Hey, you need to make a world edit selection first! pull out the wooden axe and use that, or type //pos1 and //pos2!");
+				event.setCancelled(true);
+				return;
+			}
+			if(!(sel instanceof CuboidSelection)){
+				sendMessage(p,"Please dont use anything but a cuboid selection (//sel cuboid). It makes me cry if you dont!");
+				event.setCancelled(true);
+				return;
+			}
+			Selection playersel = playerAreas.get(p.getName());
+			if(playersel == null){
+				sendMessage(p,"Hey, you dont have a registered plot! type /claimplot to get your own world edit area!");
+				event.setCancelled(true);
+				return;
+			}
+			boolean contains = (playersel.contains(sel.getMaximumPoint())&&playersel.contains(sel.getMinimumPoint()));
+			if(!contains){
+				sendMessage(p, "Hey, your world edit selection isnt contained within your area! you cant edit an area outside of your plot.");
+				event.setCancelled(true);
+				return;
+			} else {
+				if(sel.getArea()>10000){
+					
 				}
 			}
 		}
@@ -268,21 +329,23 @@ public class WorldEditHookModule extends PSCmdExe {
 	}
 	private boolean canModify(Location l, Player p){
 		if(p.hasPermission("pppopp3.creativeadmin")){
+			System.out.println(2);
 			return true;
 		}
 		if(l.getBlock().getType()==Material.BEDROCK){
 			return false;
 		}
 		for(String p2 : playerAreas.keySet()){
-			if(p2.equals(p.getName())){
-				continue;
-			} else {
-				Selection s = playerAreas.get(p2);
-				if(s.contains(l)){
+			Selection s = playerAreas.get(p2);
+			if(s.contains(l)){
+				if(p2.equals(p.getName())){
+					continue;
+				} else {
 					return false;
 				}
 			}
 		}
+		System.out.println(1);
 		return true;
 	}
 }
