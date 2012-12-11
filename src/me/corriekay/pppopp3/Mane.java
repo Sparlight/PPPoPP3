7577cc2e5e4ac6262d45bd4384cb6bef6e7b23aa
CorriePie <corriekay.pinkie@outlook.com>
2012-12-10 22:56:38 -0800

    update
    Signed-off-by: CorriePie <corriekay.pinkie@outlook.com>

--- src/me/corriekay/pppopp3/Mane.java
+++ src/me/corriekay/pppopp3/Mane.java
@@ -15,6 +15,7 @@ import me.corriekay.pppopp3.modules.WorldEditHookModule;
 import me.corriekay.pppopp3.ponymanager.PonyManager;
 import me.corriekay.pppopp3.ponyville.Ponyville;
 import me.corriekay.pppopp3.utils.PSCmdExe;
+import me.corriekay.pppopp3.warp.WarpHandler;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -41,6 +42,7 @@ public class Mane extends JavaPlugin{
 		try{modules.add(new ChatHandler());}catch(Exception e){e.printStackTrace();}
 		modules.add(new PonySpy());
 		modules.add(new AntiDiscordModule());
+		try{modules.add(new WarpHandler());}catch(Exception e){e.printStackTrace();}
 	}
 	public void onDisable(){
 		for(PSCmdExe exe : modules){
