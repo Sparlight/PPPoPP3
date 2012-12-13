6c03798460bea561b12a8526f89068b4cba8b192
CorriePie <corriekay.pinkie@outlook.com>
2012-12-12 16:53:07 -0800

    Added warp handler system
    Signed-off-by: CorriePie <corriekay.pinkie@outlook.com>

---
+++ src/me/corriekay/pppopp3/warp/QueuedWarp.java
@@ -0,0 +1,53 @@
+package me.corriekay.pppopp3.warp;
+
+import java.util.List;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Flying;
+import org.bukkit.entity.Monster;
+import org.bukkit.entity.Player;
+
+public class QueuedWarp {
+
+	private String player;
+	private Location teleport;
+	private int countdown;
+
+	public QueuedWarp(String player, Location teleport, int countdown){
+		this.player = player;
+		this.teleport = teleport;
+		this.countdown = countdown;
+	}
+	public boolean countdown(){
+		countdown--;
+		if(countdown<=3&&countdown>0){
+			Player player = Bukkit.getPlayerExact(this.player);
+			if(player == null){
+				return true;
+			} else {
+				player.sendMessage(ChatColor.LIGHT_PURPLE+"Pinkie Pie: "+countdown+"...");
+			}
+		}
+		if(countdown <= 0 ){
+			Player player = Bukkit.getPlayerExact(this.player);
+			if(player == null){
+				return true;
+			} else {
+				List<Entity> entities = player.getNearbyEntities(5,3,5);
+				for(Entity entity : entities){
+					if(entity instanceof Monster||entity instanceof Flying){
+						player.sendMessage(ChatColor.LIGHT_PURPLE+"Oh no! Catastrophic failure! Warp aborted! Get these monsters off of me!!");
+						return true;
+					}
+				}
+				player.teleport(teleport);
+				player.sendMessage(ChatColor.LIGHT_PURPLE+"Pinkie Pie: Success! Teleportation victory!");
+				return true;
+			}
+		}
+		return false;
+	}
+}
---
+++ src/me/corriekay/pppopp3/warp/WarpList.java
@@ -0,0 +1,34 @@
+package me.corriekay.pppopp3.warp;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+
+import org.bukkit.Location;
+
+public class WarpList {
+
+	private HashMap<String,Warp> warps = new HashMap<String,Warp>();
+	private ArrayList<String> warpNames = new ArrayList<String>();
+	
+	public WarpList(HashMap<String,Warp> warps){
+		this.warps = warps;
+		warpNames.addAll(warps.keySet());
+		Collections.sort(warpNames);
+	}
+	protected ArrayList<String> warps(){
+		return warpNames;
+	}
+	protected int size(){
+		return warpNames.size();
+	}
+	protected Location getWarp(String warpName){
+		Location loc;
+		try {
+			loc = warps.get(warpName).loc();
+		} catch (NullPointerException e) {
+			return null;
+		}
+		return loc;
+	}
+}
